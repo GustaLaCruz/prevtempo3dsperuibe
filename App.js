34b31e519d8182289099ad7d50561f74cb612cb6
Gustavo La Cruz <48767935+GustaLaCruz@users.noreply.github.com>
2022-08-04 09:29:03 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Touchable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={styles.bloco}>
+        <Text style={styles.titulo}>Wettervorhersage</Text>
+        </View>
+
+        <View style={styles.blocoConteudo}>
+        <Text style={styles.textoLabel}>Geben Sie Ihre Stadt ein
+:</Text>
+        <TextInput
+          placeholder='deine Stadt ...'
+          style={styles.input}
+        />
+        </View>
+
+        <View>
+            <TouchableOpacity style={styles.botao}>
+              <Text style={styles.textoBotao}>Suche</Text>
+            </TouchableOpacity>
+        </View>
+
     </View>
   );
 }
@@ -13,8 +30,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  bloco:{
+    alignItems:'center',
+    marginBottom:20,
+    marginTop:20
+  },
+  blocoConteudo:{
+    
+  },
+  titulo:{
+    fontSize:30
+  },
+  textoLabel:{
+    fontSize:20,
+    marginLeft:'10%'
+  },
+  input:{
+    marginLeft:'10%',
+    borderBottomWidth:2,
+    marginRight:'10%',
+    fontSize:20
+    
+  },
+  botao:{
+    marginLeft:'10%',
+    marginRight:'10%',
+    marginTop:'10%',
+    backgroundColor:'#DF330E'
+  },
+  textoBotao:{
+    fontSize:20,
+    textAlign:'center'
   },
 });
